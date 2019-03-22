7e21bbc7dfb598dd688d722beda4becf48b3aaeb
DisturbeD-347939 <ricardo.graca17@bathspa.ac.uk>
2019-03-22 22:37:32 +0000

    First commit

---
+++ hello.js
@@ -0,0 +1,52 @@
+var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+var Twitter = require('twitter');
+var fs = require('fs');
+
+//Authentication into the Personality Insight API
+
+var personalityInsights = new PersonalityInsightsV3({
+  version: '2019-03-22',
+  iam_apikey: 'YMpOWrKJysOg9xdNt3HNIcpnVqZHve-_VaVKkq4cokxi',
+  url: 'https://gateway-lon.watsonplatform.net/personality-insights/api'
+});
+
+//Authentication into Twitter
+
+var client = new Twitter({
+  consumer_key: 'PX8hDcf9YHAKqXII5TdhlHMVw',
+  consumer_secret: 'FFb7qceLkOXSa0GnLMgzItSeIYsmpckxToTZe0jUHPVMRNeWPV',
+  access_token_key: '3135731542-Amvbgvz8s1t0v80KW4TCLnoFfNzUIBSJuWwXvWM',
+  access_token_secret: 'Bja7LysYPFqkMubC8i9d8R1GljlTozLjtWtIRu52BVz6F'
+});
+
+var output = "";
+
+client.get('statuses/user_timeline', {screen_name: 'DisturbedBoy69', count: '5', include_rts: 'false'} , function(error, tweets, response)
+{
+    var data = JSON.stringify(tweets, null, 2);
+    let profileText = JSON.parse(data);
+    for(var i = 0; i < profileText.length;   i++)
+    {
+        output +=  profileText[i].text;
+    }
+    console.log(output);
+    //fs.writeFileSync('profile.json', JSON.parse(output));
+})
+
+//Imports profile.json file.
+
+var profileParams = {
+  // Get the content from the JSON file.
+  content: require('./profile.json'),
+  content_type: 'application/json'
+};
+
+//Process profile.json file.
+
+personalityInsights.profile(profileParams, function(error, profile) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.stringify(profile, null, 2));
+  }
+});
