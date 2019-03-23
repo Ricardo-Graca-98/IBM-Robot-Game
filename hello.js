ba22568da6c9ad003fe2c2513db899f309c0f49c
DisturbeD-347939 <ricardo.graca17@bathspa.ac.uk>
2019-03-23 15:42:08 +0000

    Updated hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
 var Twitter = require('twitter');
 var fs = require('fs');
+const simpleGit = require('simple-git');
 
 //Authentication into the Personality Insight API
 
@@ -19,34 +20,48 @@ var client = new Twitter({
   access_token_secret: 'Bja7LysYPFqkMubC8i9d8R1GljlTozLjtWtIRu52BVz6F'
 });
 
-var output = "";
+var text = "";
+var outputText = "";
 
-client.get('statuses/user_timeline', {screen_name: 'DisturbedBoy69', count: '5', include_rts: 'false'} , function(error, tweets, response)
+//Get tweets from user timeline
+client.get('statuses/user_timeline', {screen_name: 'DisturbedBoy69', count: '100', include_rts: 'false'} , function(error, tweets, response)
 {
+    //Make them json
     var data = JSON.stringify(tweets, null, 2);
+    //Clean them
     let profileText = JSON.parse(data);
+    outputText += "{ \"contentItems\" : [\n\n";
+    //Take tweet text
     for(var i = 0; i < profileText.length;   i++)
     {
-        output +=  profileText[i].text;
+        text =  profileText[i].text;
+        text = text.replace(/(\r\n|\n|\r)/gm," ");
+        
+        outputText += "{\n\t\"content\":\"" + text + "\",\n\t\"contenttype\": \"text/plain\",\n\t\"created\":" + 0 + ",\n\t\"id\":\"" + profileText[i].id + "\",\n\t\"language\":\"en\"\n}";
+        
+        if((i+1) != profileText.length)
+            {
+                outputText += ",\n";
+            }
     }
-    console.log(output);
-    //fs.writeFileSync('profile.json', JSON.parse(output));
+    outputText += "]}";
+    fs.writeFileSync('./profile.json', outputText);
+    var profileParams = {
+        // Get the content from the JSON file.
+        content: require('./profile.json'),
+        content_type: 'application/json',
+        consumption_preferences: 'true'
+    };
+    personalityInsights.profile(profileParams, function(error, profile) {
+        if (error) 
+        {
+            console.log(error);
+        } 
+        else 
+        { 
+            fs.writeFileSync('./outputUnity.json', JSON.stringify(profile, null, 2));
+        }
+    });
+    simpleGit.push('origin', 'master');
 })
 
-//Imports profile.json file.
-
-var profileParams = {
-  // Get the content from the JSON file.
-  content: require('./profile.json'),
-  content_type: 'application/json'
-};
-
-//Process profile.json file.
-
-personalityInsights.profile(profileParams, function(error, profile) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.stringify(profile, null, 2));
-  }
-});
