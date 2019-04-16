5803578e6e81dd14da9e49fc7da6927294df0b2a
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-04-16 14:36:12 +0100

    Changed feedback

--- Server.js
+++ Server.js
@@ -12,7 +12,7 @@ http.createServer(function (req, res) {
         if (err) 
         {
             res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
+            return res.end("Not in the database, sorry!");
         } 
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
