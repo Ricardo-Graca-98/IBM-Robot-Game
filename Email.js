71bb3dd275992d4c605469230d752b4c1ae1bd1e
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-05-15 01:25:41 +0100

    Updated email.js

--- Email.js
+++ Email.js
@@ -1,10 +1,15 @@
 var nodemailer = require('nodemailer');
 var fs = require('fs');
 
+require('dns').lookup(require('os').hostname(), function (err, add, fam) 
+{
+  var ip = add;
+  console.log(ip);
+})
+
 var Credentials = fs.readFileSync('Keys.json', 'utf-8');
 var ParsedCredentials = JSON.parse(Credentials);
-
-
+var checkEmails = fs.readFileSync('sendAuth.txt', 'utf8');
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -13,21 +18,29 @@ var transporter = nodemailer.createTransport({
       pass: ParsedCredentials.NiceBotsEmail[0].key
     }
   });
-  
-  var mailOptions = {
-    from: 'nicebotsIBM@gmail.com',
-    to: 'ricardo.graca17@bathspa.ac.uk',
-    subject: 'Verify your email - NiceBots',
-    text: 'Please verify your email cunt!'
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error)
+
+  //setTimeout(sendEmail, 100);
+
+  function sendEmail(email, name)
+  {
+    var mailOptions = 
     {
-      console.log(error);
-    }
-    else
-    {
-      console.log('Email sent: ' + info.response);
-    }
-  }); 
+      from: 'nicebotsIBM@gmail.com',
+      to: email,
+      subject: 'Verify your email - NiceBots',
+      text: 'Please verify your email by accessing this link -> ' + name
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error)
+      {
+        console.log(error);
+      }
+      else
+      {
+        console.log('Email sent: ' + info.response);
+      }
+    }); 
+  }
+  
+  
