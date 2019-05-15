0d9b2a7ca86a2bfb66c1278c759840987d515fcb
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-05-15 15:10:04 +0100

    Streaming direct messages

--- Authentication.js
+++ Authentication.js
@@ -5,25 +5,27 @@ var Twit = require('twit');
 var Credentials = fs.readFileSync('Keys.json', 'utf-8');
 var ParsedCredentials = JSON.parse(Credentials);
 
-//Authentication into Twitter
-var client = new Twitter
-({
-    access_token_secret: ParsedCredentials.twitter[0].access_token_secret,
-    access_token_key: ParsedCredentials.twitter[0].access_token_key,
-    consumer_secret: ParsedCredentials.twitter[0].consumer_secret,
-    consumer_key: ParsedCredentials.twitter[0].consumer_key
-});
-
+//Twitter authentication
 var T = new Twit({
-    consumer_key: ParsedCredentials.twitter[0].consumer_key,
-    consumer_secret: ParsedCredentials.twitter[0].consumer_secret,
-    access_token: ParsedCredentials.twitter[0].access_token_key,
-    access_token_secret: ParsedCredentials.twitter[0].access_token_secret,
+    consumer_key: ParsedCredentials.twitter[2].consumer_key,
+    consumer_secret: ParsedCredentials.twitter[2].consumer_secret,
+    access_token: ParsedCredentials.twitter[2].access_token_key,
+    access_token_secret: ParsedCredentials.twitter[2].access_token_secret,
   })
 
-setTimeout(sendMessage, 100);
+setTimeout(setup, 0);
 
-function sendMessage()
+function setup()
+{
+    var IDs = fs.readFileSync('sendAuth.txt', 'utf-8');
+    var parsedIDs = IDs.split("\n");
+    for(var i = 0; i < parsedIDs.length; i++)
+    {
+        //setTimeout(sendMessage, 0, parsedIDs[i]);
+    }
+}
+
+function sendMessage(ID)
 {
   var replyTo = 
   {
@@ -34,11 +36,28 @@ function sendMessage()
           {
               'target': 
               {
-                'id': 'disturbedboy69'
+                'recipient_id': ID
               },
               'message_data': 
               {
-                'text': 'You are going to be killed by robots one day!',
+                'text': 'Hello! I\'m here to confirm that you created an account in our game! Please type yes or no to confirm' ,
+                'quick_reply': 
+                {
+                  'type': 'options',
+                  'options': 
+                  [
+                    {
+                      'label' : 'Yes',
+                      'description' : 'Confirm it was you',
+                      'metadata' : 'external_id_1'
+                    },
+                    {
+                      'label' : 'No',
+                      'description' : 'It wasn\'t me',
+                      'metadata' : 'external_id_2'
+                    }
+                  ]
+                }
               }
         }
       }
@@ -46,6 +65,17 @@ function sendMessage()
 
   T.post('direct_messages/events/new', replyTo, function(err,data,response)
   {
-    console.info(data);
+      console.info(data);
   });
 }
+
+setTimeout(checkIfWorks, 500);
+
+function checkIfWorks()
+{
+  T.stream('user').on('direct_message', function (eventMsg) 
+  {
+
+  });
+}
+
