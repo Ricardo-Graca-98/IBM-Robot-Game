ebd9b741c46b1a5e591c08930c215ef3d42c25f0
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-05-15 14:11:52 +0100

    Updated auth

--- Authentication.js
+++ Authentication.js
@@ -21,35 +21,31 @@ var T = new Twit({
     access_token_secret: ParsedCredentials.twitter[0].access_token_secret,
   })
 
-var replyTo = 
-{
-    'event': 
-    {
-        'type': 'message_create',
-        'message_create': 
-        {
-            'target': 
-            {
-              'recipient_id': '749888387885240320'
-            },
-            'message_data': 
-            {
-              'text': 'You are going to be killed by robots one day!',
-            }
-      }
-    }
-}
-
 setTimeout(sendMessage, 100);
 
 function sendMessage()
 {
-    client.get('statuses/user_timeline', {screen_name: 'TheOnlySpite', count: '1000', include_rts: 'false'} , function(error, tweets, response)
-    {
-        //console.log(tweets);
-    })
-    T.post('direct_messages/events/new', replyTo, function(err,data,response)
-    {
-        console.info(data);
-    });
+  var replyTo = 
+  {
+      'event': 
+      {
+          'type': 'message_create',
+          'message_create': 
+          {
+              'target': 
+              {
+                'id': 'disturbedboy69'
+              },
+              'message_data': 
+              {
+                'text': 'You are going to be killed by robots one day!',
+              }
+        }
+      }
+  }
+
+  T.post('direct_messages/events/new', replyTo, function(err,data,response)
+  {
+    console.info(data);
+  });
 }
