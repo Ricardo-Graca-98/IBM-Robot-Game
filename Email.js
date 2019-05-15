66203176f88e9fab1c374cf37631c8153b96ecc1
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-05-15 13:53:03 +0100

    Formatted email.js

--- Email.js
+++ Email.js
@@ -11,36 +11,34 @@ var Credentials = fs.readFileSync('Keys.json', 'utf-8');
 var ParsedCredentials = JSON.parse(Credentials);
 var checkEmails = fs.readFileSync('sendAuth.txt', 'utf8');
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: ParsedCredentials.NiceBotsEmail[0].email,
-      pass: ParsedCredentials.NiceBotsEmail[0].key
-    }
-  });
-
-  //setTimeout(sendEmail, 100);
-
-  function sendEmail(email, name)
+var transporter = nodemailer.createTransport
+({
+  service: 'gmail',
+  auth: 
   {
-    var mailOptions = 
-    {
-      from: 'nicebotsIBM@gmail.com',
-      to: email,
-      subject: 'Verify your email - NiceBots',
-      text: 'Please verify your email by accessing this link -> ' + name
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error)
-      {
-        console.log(error);
-      }
-      else
-      {
-        console.log('Email sent: ' + info.response);
-      }
-    }); 
+    user: ParsedCredentials.NiceBotsEmail[0].email,
+    pass: ParsedCredentials.NiceBotsEmail[0].key
   }
-  
-  
+});
+
+//setTimeout(sendEmail, 100);
+function sendEmail(email, name)
+{
+  var mailOptions = 
+  {
+    from: 'nicebotsIBM@gmail.com',
+    to: email,
+    subject: 'Verify your email - NiceBots',
+    text: 'Please verify your email by accessing this link -> ' + name
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error)
+    {
+      console.log(error);
+    }
+    else
+    {
+      console.log('Email sent: ' + info.response);
+    }
+  }); 
+}
