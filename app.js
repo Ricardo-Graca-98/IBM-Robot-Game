66816690c412c123d28232679f0b636a51e96f26
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2019-05-27 20:36:31 +0100

    Added changing module functionality

--- app.js
+++ app.js
@@ -95,6 +95,56 @@ function handler (req, res)
         }
         return res.end("Adding stats to " + stats[0] + "! " + debug);
     }
+    else if(requestChar == "$")
+    {
+        var data = requestText.split("$");
+        var name = data[0];
+        var modData = data[1];
+        var modName = modData.slice(0, modData.length-1);
+        var modLvl = modData[modData.length-1];
+        var modules = fs.readFileSync('./Users/' + name + '/robotStats.txt', 'utf-8');
+        modName = modName.charAt(0).toUpperCase() + modName.slice(1);
+        //var lvlPos = modName.length + modules.indexOf(modName);
+        var readModuleInfo = fs.readFileSync('./Modules/' + modName + '/LVL' + modLvl + '.txt', 'utf-8');
+        for(var i = 0; i < readModuleInfo.length; i++)
+        {
+            var number = "";
+            if(isNaN(readModuleInfo[i]) == 0)
+            {
+                while(readModuleInfo[i].match(/^[0-9]+$/) != null)
+                {
+                    number += readModuleInfo[i];
+                    i++;
+                }
+            }
+            if(number != "")
+            {
+                break;
+            }
+        }
+        var stats = modules.split("/");
+        for(var i = 0; i < stats.length; i++)
+        {
+            var statsName = stats[i].slice(0, stats[i].length-1)
+            if(statsName == modName)
+            {
+                stats[i] = stats[i].slice(0, stats[i].length-1);
+                stats[i] += modLvl;
+                console.log(stats[i]);
+                stats[i+1] = number;
+                console.log(stats[i+1]); 
+            }
+        }
+        fs.writeFileSync('./Users/' + name + '/robotStats.txt', "");
+        for(var i = 0; i < stats.length; i++)
+        {
+            fs.appendFileSync('./Users/' + name + '/robotStats.txt', stats[i]);
+            if(i+1 < stats.length)
+            {
+                fs.appendFileSync('./Users/' + name + '/robotStats.txt', "/");
+            }
+        }
+    }
     else if(requestChar == null)
     {
         fs.readFile(__dirname + '/index.html',
